0f09a16b854f445d38de1bf433f10a26bbfefe0c
PedroAz <pdegutty@gmail.com>
2022-11-24 09:12:38 -0300

    swagger

---
+++ aluraflix/tests/test_authentication_.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from django.contrib.auth import authenticate
+from django.urls import reverse
+from rest_framework import status
+
+class AuthenticationUserTestCase(APITestCase):
+
+    def setUp(self):
+        #teste de gets, delete etc..
+        self.list_url = reverse('programas-list')
+        self.user = User.objects.create_user('c3po', password='123456')
+
+    def test_autenticacao_user_com_credenciais_corretas(self):
+        """teste que verifica a autenticação de um user com as credenciais corretas"""
+        user = authenticate(username='c3po', password='123456')
+        self.assertTrue((user is not None) and user.is_authenticated)
+
+    def test_requesicao_get_nao_autorizada(self):
+        """Teste que verifica uma requisição GET de um user sem autenticar"""
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_autenticacao_de_user_com_username_incorreto(self):
+        user = authenticate(username='c3pp', password='123456')
+        self.assertFalse((user is not None) and user.is_authenticated)
+
+    def test_requisicao_get_com_user_autenticado(self):
+        """Teste que verifica uma requisição GET de um user autenticado"""
+        self.client.force_authenticate(self.user)
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
